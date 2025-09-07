59d0246c86507646815ee5542b3d6227414c625d
ersmax <ersmax@gmail.com>
2025-09-07 19:22:20 +0200

    Fix errors

--- chapt1/1.1_Introduction/playground.cpp
+++ chapt1/1.1_Introduction/playground.cpp
@@ -2,11 +2,14 @@
 // Created by admin on 07/09/2025.
 //
 #include <string>
+#include <iostream>
 using namespace std;
 
 int main() {
     string fruit;
     fruit = "durian";
+    string s = R"(\t\\t\n)";
+    cout << s << endl;
     return 0;
 }
 
