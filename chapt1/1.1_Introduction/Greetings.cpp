ea90068b51e9510f3c73cdc255e16d572cc9df04
ersmax <e.masarin@student.vu.nl>
2025-09-07 12:39:25 +0200

    Greetings program

--- chapt1/1.1_Introduction/Greetings.cpp
+++ chapt1/1.1_Introduction/Greetings.cpp
@@ -1,3 +1,23 @@
 //
 // Created by admin on 07/09/2025.
 //
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This program greets the user
+    int numberOfLanguages;
+
+    cout << "Hello reader.\n"
+         << "Welcome to C++.\n";
+
+    cout << "How many programming languages have you used?";
+    cin >> numberOfLanguages;
+
+    if (numberOfLanguages < 1)
+        cout << "Welcome to C++.";
+    else
+        cout << "Congrats to your new learning";
+
+    return 0;
+}
