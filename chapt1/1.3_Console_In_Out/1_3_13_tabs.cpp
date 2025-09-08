e365a562e30006131dbceb0341efb8402d23d10a
ersmax <e.masarin@student.vu.nl>
2025-09-08 16:23:52 +0200

    Add ex 1.3.13

---
+++ chapt1/1.3_Console_In_Out/1_3_13_tabs.cpp
@@ -0,0 +1,15 @@
+/*
+Give an output statement that produces the letter 'A', followed by the newline
+character, followed by the letter 'B', followed by the tab character, followed by
+the letter 'C'.
+*/
+#include <iostream>
+using namespace std;
+
+int main( ) {
+    cout << 'A' << '\n'
+         << 'B' << '\t'
+         << "C\n";
+
+    return 0;
+}
