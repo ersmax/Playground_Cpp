3b14aca4288cfd43c4f4cb41e54deb25e2c92068
ersmax <e.masarin@student.vu.nl>
2025-09-08 16:27:08 +0200

    Add ex 1.3.14 full name age

---
+++ chapt1/1.3_Console_In_Out/1_3_14_Fix_error.cpp
@@ -0,0 +1,29 @@
+/*
+The following code intends to input a user’s first name, last name, and age.
+However, it has an error. Fix the code.
+string fullName;
+int age;
+cout << "Enter your first and last name." << endl;
+cin >> fullName;
+cout << "Enter your age." << endl;
+cin >> age;
+cout << "You are " << age << " years old, " << fullName << endl;
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ( ) {
+    string firstName, lastName;
+    int age;
+
+    cout << "Enter your first and last name." << endl;
+    cin >> firstName >> lastName;   // Fix: Separate first and last names
+    cout << "Enter your age." << endl;
+    cin >> age;
+    cout << "You are " << age << " years old, "
+         << firstName << " " << lastName << endl; // Fix: Output both names
+
+    return 0;
+}
