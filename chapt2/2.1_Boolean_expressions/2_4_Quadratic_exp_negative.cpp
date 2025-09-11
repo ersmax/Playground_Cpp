7a1ea01d04ebc6c3bc4146e275a36b17190bde5b
ersmax <e.masarin@student.vu.nl>
2025-09-11 15:53:51 +0200

    Add 2.4 Quadratic expression negative

---
+++ chapt2/2.1_Boolean_expressions/2_4_Quadratic_exp_negative.cpp
@@ -0,0 +1,15 @@
+/*
+Consider the quadratic expression
+x2 − 4x + 3
+Describing where this quadratic is negative involves describing a set of numbers
+that are simultaneously greater than the smaller root (1) and less than the larger
+root (3). Write a C++ Boolean expression that is true when the value of this
+quadratic is negative.
+*/
+
+/*
+My answer:
+x > 1 && x < 3
+Parentheses are optional due to operator precedence:
+The relational operators (`>`, `<`) have higher precedence than the logical AND operator (`&&`).
+*/
