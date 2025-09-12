4e98b505a2375426ba84d3db71f23125468d0e21
ersmax <e.masarin@student.vu.nl>
2025-09-12 11:12:17 +0200

    Fix 2.2 Boolean exp

--- chapt2/2.1_Boolean_expressions/2_2_Boolean_interval.cpp
+++ chapt2/2.1_Boolean_expressions/2_2_Boolean_interval.cpp
@@ -13,6 +13,8 @@ In C++, the expression `2 < x < 3` is evaluated in two steps due to
 operator precedence and associativity.
 First, `2 < x` is evaluated, which results in a Boolean value (true or false).
 Then, this Boolean value is compared to `3`, which is not the intended behavior.
+In fact, either true (1) or false (0) will always be less than 3,
+making the entire expression always true regardless of the value of `x`.
 The correct way to express that `x` lies between `2` and `3` is to use
 the logical AND operator (`&&`) to combine two comparisons: `2 < x && x < 3`.
 */
