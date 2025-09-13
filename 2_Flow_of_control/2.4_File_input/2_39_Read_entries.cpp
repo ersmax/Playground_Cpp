63ee82d4c93e92e4faa8363a9db79ccd2398d60a
ersmax <e.masarin@student.vu.nl>
2025-09-13 15:10:57 +0200

    Add 2.39 Read entries

---
+++ 2_Flow_of_control/2.4_File_input/2_39_Read_entries.cpp
@@ -0,0 +1,46 @@
+/*
+Consider a file of high scores. The first line is an integer that stores the number
+of records in the file. The records alternate between the name of the player (first
+name only) and the player’s score. Here is a sample file named scores.txt
+consisting of three entries:
+3
+Gordon
+500
+Mario
+550
+Illidan
+385.
+Write code that reads the file and outputs the player’s name and score on one
+line, e.g.,
+Gordon, 500
+Mario, 550
+Illidan, 385
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main ( ) {
+    fstream inputStream;
+    int scores;
+
+    inputStream.open("99_Text_files/1_scores.txt");
+    if (!inputStream.is_open()) {
+        cout << "Failed to open the file" << endl;
+        return -1;
+    }
+    
+    inputStream >> scores;
+
+    for (int idx = 0; idx < scores; idx++) {
+        string name;
+        int score;
+        inputStream >> name;
+        inputStream >> score;
+        cout << name << ", " << score << endl;
+    }
+    inputStream.close();
+
+    return 0;
+}
