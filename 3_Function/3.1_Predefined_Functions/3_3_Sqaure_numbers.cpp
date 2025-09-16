065a64dade90001af93b817b0c05e6e293d806a1
ersmax <e.masarin@student.vu.nl>
2025-09-16 18:08:48 +0200

    Add 3.3 Square root of numbers

---
+++ 3_Function/3.1_Predefined_Functions/3_3_Sqaure_numbers.cpp
@@ -0,0 +1,19 @@
+/*
+Write a complete C++ program to compute and output the square roots of the
+whole numbers 1 to 10.
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main( ) {
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
+    for (int num = 1; num < 11; ++num)
+        cout << "Square root of "
+            << num << ": " << sqrt(num) << endl;
+    
+    return 0;
+}
