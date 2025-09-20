f78865f00e471caca383f7354970e834e7a5b883
ersmax <e.masarin@student.vu.nl>
2025-09-20 16:47:33 +0200

    Add function definition inside another def

---
+++ 3_Function/3.2_Programmer_defined_function/3_10_Function_definition.cpp
@@ -0,0 +1,10 @@
+/*
+Can a function definition appear inside the body of another function
+definition?
+*/
+// No, a function definition cannot appear inside the body of another function definition.
+// A function definition must be at the top level of a file or in a namespace.
+// Nesting function definitions is not allowed in C++.
+// Alternatively, a function can be declared (function prototype) before it is used,
+// and then defined later in the file or in a separate file.
+// This allows for better organization and modularity of code.
