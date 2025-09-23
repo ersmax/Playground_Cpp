e2f63e024ca642272c27b443c9f88ca53d77a6ad
ersmax <e.masarin@student.vu.nl>
2025-09-23 22:21:39 +0200

    Add temperature conv

---
+++ 3_Function/3.2_Programmer_defined_function/3_0_1_Temp_conversion.cpp
@@ -0,0 +1,40 @@
+/*
+This program illustrate the use of two functions
+to compute and show the temperature conversion.
+*/
+#include <iostream>
+using namespace std;
+
+double tempConversion(double cDegrees);
+double insertTemperature();
+void showResult(double fDegrees, double cDegrees);
+
+int main( ) {
+    double celsius = insertTemperature( );
+    double fahrenheit = tempConversion(celsius);
+    showResult(fahrenheit, celsius);
+    return 0;
+}
+
+double tempConversion(double cDegrees) {
+    return (9/5.0) * cDegrees + 32.0;
+}
+
+double insertTemperature() {
+    double celsius;
+    cout << "Insert temperature in C:";
+    cin >> celsius;
+    return celsius;
+}
+
+void showResult(double fDegrees, double cDegrees) {
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << cDegrees << " degrees Celsius is "
+         << fDegrees << " degrees Fahrenheit.\n";
+}
+
+
+
+
