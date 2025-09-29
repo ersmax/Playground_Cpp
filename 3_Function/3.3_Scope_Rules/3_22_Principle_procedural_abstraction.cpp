fa5e4370204465f7c2bd2ce4cd6c2891f763f9eb
ersmax <e.masarin@student.vu.nl>
2025-09-29 15:58:53 +0200

    Add comments

--- 3_Function/3.3_Scope_Rules/3_22_Principle_procedural_abstraction.cpp
+++ 3_Function/3.3_Scope_Rules/3_22_Principle_procedural_abstraction.cpp
@@ -4,9 +4,6 @@ What is the purpose of the comment that accompanies a function declaration?
 // The comment explains the function's purpose, its parameters, and its return value.
 // It helps with information hiding and procedural abstraction, making the function
 // to behave like a black box.
-// Formally speaking:
-// The principle of procedural abstraction says that a function should be written so
-// that it can be used like a black box. This means that the programmer who uses
-// the function needs not look at the body of the function definition to see how the
-// function works. The function declaration and accompanying comment should be
-// all the programmer needs in order to use the function.
+// Formally:
+// The comment explains what action the function takes, including any value returned,
+// and gives any other information that you need to know in order to use the function.
---
+++ 3_Function/3.3_Scope_Rules/3_23_Procedural_abstraction.cpp
@@ -0,0 +1,15 @@
+/*
+What is the principle of procedural abstraction as applied to function
+definitions?
+*/
+// The principle of procedural abstraction is that the details of how a function
+// accomplishes its task are hidden from the user of the function. The user only
+// needs to know the function's name, its parameters, and what it returns (if
+// anything). This allows the user to call the function without needing to
+// understand its internal workings, promoting code reuse and modularity.
+// Formally speaking:
+// The principle of procedural abstraction says that a function should be written so
+// that it can be used like a black box. This means that the programmer who uses
+// the function needs not look at the body of the function definition to see how the
+// function works. The function declaration and accompanying comment should be
+// all the programmer needs in order to use the function.
