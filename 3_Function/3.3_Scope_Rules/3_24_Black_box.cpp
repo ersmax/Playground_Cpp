2a3524b5753ddd1f60aced66ecf3cf73d33c9b06
ersmax <e.masarin@student.vu.nl>
2025-09-29 16:01:47 +0200

    Add function black box

---
+++ 3_Function/3.3_Scope_Rules/3_24_Black_box.cpp
@@ -0,0 +1,20 @@
+/*
+What does it mean when we say the programmer who uses a function should
+be able to treat the function like a black box? (This question is very closely
+related to the previous question.)
+*/
+
+// When we say that a programmer should be able to treat a function like a black box,
+// we mean that the programmer can use the function without needing to understand
+// its internal workings or implementation details.
+// The focus is on the function's interface, which includes its name,
+// parameters, return type, and behavior (what it does),
+// rather than how it achieves that behavior.
+// This allows for abstraction, making it easier to use and reuse functions
+// without getting bogged down in the complexities of their internal logic.
+// Formally speaking:
+// When we say that the programmer who uses a function should be able to treat the
+// function like a black box, we mean the programmer should not need to look at
+// the body of the function definition to see how the function works. The function
+// declaration and accompanying comment should be all the programmer needs in
+// order to use the function.
