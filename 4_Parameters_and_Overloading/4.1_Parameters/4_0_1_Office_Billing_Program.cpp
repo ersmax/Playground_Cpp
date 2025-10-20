c91bcca65bd41367e37883cfb953946143dcb2ac
ersmax <e.masarin@student.vu.nl>
2025-10-20 13:29:36 +0200

    Add office billing

--- 4_Parameters_and_Overloading/4.1_Parameters/4_0_1_Office_Billing_Program.cpp
+++ 4_Parameters_and_Overloading/4.1_Parameters/4_0_1_Office_Billing_Program.cpp
@@ -1,3 +1,53 @@
-//
-// Created by admin on 20/10/2025.
-//
+/*
+This program computes the bill due to
+startting from hours and minutes
+passed by value.
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::ios;
+
+
+const double RATE = 150.00;
+
+double fee(int hoursWorked, int minutesWorked);
+// Precondition: hoursWorked and minutesWorked are non-negative integers
+// Postcondition: returns the fee for legal service
+//               based on the time worked
+
+int main( ) {
+    // Postcondition: Prompts user for hours and minutes worked,
+    //                then displays the bill due.
+    int hours, minutes;
+    double bill;
+
+    cout << "Enter the hours and minutes\n"
+         << "of legal service\n"
+         << "(separated by a space):";
+    cin >> hours >> minutes;
+
+    bill = fee(hours, minutes);
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << "For " << hours << " hours and "
+         << minutes << " minutes, your bill is $"
+         << bill << "\n";
+
+    return 0;
+}
+
+double fee(int hoursWorked, int minutesWorked) {
+    // Local variables: hoursWorked, minutesWorked, quarterHours
+    int quarterHours;
+
+    minutesWorked = hoursWorked * 60 + minutesWorked;
+    quarterHours = minutesWorked / 15;
+    return (quarterHours * RATE);
+}
+
+
+
