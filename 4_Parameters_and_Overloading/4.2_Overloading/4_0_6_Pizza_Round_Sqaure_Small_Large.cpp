3b85ca8d8ca41ba7d5ce2912a5e24972d7f8f982
ersmax <e.masarin@student.vu.nl>
2025-10-26 11:48:36 +0100

    Add setup Pizza overload

---
+++ 4_Parameters_and_Overloading/4.2_Overloading/4_0_6_Pizza_Round_Sqaure_Small_Large.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios;
+
+void getData(int& smallDiameter, int& largeDiameter,
+            double& priceSmall, double& priceLarge);
+
+void getData(int& smallLength, int& smallWidth,
+             int& largeLength, int& largeWidth,
+             double& priceSmall, double& priceLarge);
+
+double unitPrice(int diameter, double price);
+
+double unitPrice(int length, int width, double price);
+
+int main( ) {
+    int diameterSmall, diameterLarge,
+        lengthSmall, widthSmall,
+        lengthLarge, widthLarge;
+    double priceRoundSmall, priceRoundLarge,
+           pRectangularSmall, pRectangularLarge;
+
+    cout << "Welcome to Pizza Hut.\n";
+    getData(diameterSmall, diameterLarge,
+            priceRoundSmall, priceRoundLarge);
+    getData(lengthSmall, widthSmall,
+            lengthLarge, widthLarge,
+            pRectangularSmall, pRectangularLarge);
+
+
+
+
+
+    return 0;
+}
+
+
+void getData(int& smallDiameter, int& largeDiameter,
+             double& priceSmall, double& priceLarge) {
+
+    cout << "Enter the diameter and price of a SMALL round pizza\n "
+        << "(separated by space, e.g. 10 8.99): ";
+    cin >> smallDiameter >> priceSmall;
+    cout << "Enter the diameter and price of a LARGE round pizza\n "
+         << "(separated by space, e.g. 12 10.99): ";
+    cin >> largeDiameter >> priceLarge;
+}
+
+
+void getData(int& smallLength, int& smallWidth,
+             int& largeLength, int& largeWidth,
+             double& priceSmall, double& priceLarge) {
+
+    cout << "Enter the length, width and price of a SMALL pizza\n"
+         << "(separated by space, e.g. 10 12 10.99): ";
+    cin >> smallLength >> smallWidth >> priceSmall;
+    cout << "Enter the length, width and price of a LARGE pizza\n"
+         << "(separated by space, e.g. 12 14 12.99): ";
+    cin >> largeLength >> largeWidth >> priceLarge;
+}
