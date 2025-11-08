d8c533914d370b60ecce9f090a1c56db4e5ba9d7
ersmax <e.masarin@student.vu.nl>
2025-11-08 12:01:17 +0100

    Add range based loop arrays

---
+++ 5_Arrays/5.1_Introduction_Arrays/5_0_1_Array_Access.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <limits>
+
+int main( ) {
+    int scores[5], scores2[5];
+    std::cout << "Enter 5 scores:\n";
+    int max = std::numeric_limits<int>::min();
+
+    for (int idx = 0; idx < std::size(scores); ++idx) {
+        std::cin >> scores[idx];
+        if (scores[idx] > max)
+            max = scores[idx];
+    }
+    std::cout << "Enter 5 scores:\n";
+    // better readability
+    for (int& score : scores2) {
+        std::cin >> score;
+        if (score > max)
+            max = score;
+    }
+
+
+    std::cout << "The highest score is " << max << "\n"
+              << "The scores and their difference from the highest are:\n";
+
+    for (size_t idx = 0; idx < std::size(scores); ++idx)
+        std::cout << scores[idx] << " off by " << (max - scores[idx]) << "\n";
+
+    std::cout << "The highest score is " << max << "\n"
+              << "The scores and their difference from the highest are:\n";
+    // better readability
+    for (const int& score : scores2)
+        std::cout << score << " off by " << (max - score) << "\n";
+
+    return 0;
+}
