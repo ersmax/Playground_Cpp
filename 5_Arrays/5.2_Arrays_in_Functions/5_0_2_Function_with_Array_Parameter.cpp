65fa1eb4c82d46fe0d956d5a7df44bf3537534c8
ersmax <e.masarin@student.vu.nl>
2025-11-11 12:13:24 +0100

    Add show array content

--- 5_Arrays/5.2_Arrays_in_Functions/5_0_2_Function_with_Array_Parameter.cpp
+++ 5_Arrays/5.2_Arrays_in_Functions/5_0_2_Function_with_Array_Parameter.cpp
@@ -2,13 +2,17 @@
 
 void fillUp(int (&a)[10]);
 // Reference to an array of (known) size 10
+// fixed binding without changing size.
+//
 // Precondition: size is the declared size of the array
 //               User will type in the array element or indexed variables
 // Postcondition: The array `a` is filled with integers from console inline
 
 
 void fillUp2(int a[], int size);
-// Notes: Array decays to pointer. No pass-by-reference here.
+// Notes: Array decays to pointer (int* a). No pass-by-reference here.
+// However, we have a dynamic array that can accept different sizes.
+//
 // Precondition: size is the declared size of the array
 //               User will type in the array element or indexed variables
 // Postcondition: The array `a` is filled with integers from console inline
@@ -18,11 +22,18 @@ void fillUp2(int a[], int size);
 //                because everything abu=out the array is told (location in memory, types of each element
 //                and hence size of each indexed variable), except for the size of the array.
 
+
+void showContent(const int a[], int sizeOfContainer);
+// Precondition: sizeOfContainer is the declared size of the array.
+//               All indexed variables of the array have been given values.
+// Postcondition: display the content of the array on screen.
+
 int main( ) {
     int myArray[10] = {};
 
     fillUp(myArray);
     fillUp2(myArray, std::size(myArray));
+    showContent(myArray, std::size(myArray));
     std::cout << "\n";
     return 0;
 }
@@ -40,4 +51,12 @@ void fillUp2(int a[], int size) {
         std::cin >> a[idx];
     std::cout << "The last array index used is: " << (size - 1) << "\n";
 }
+
+void showContent(const int a[], int sizeOfContainer) {
+    std::cout << "The array contains the following values:\n";
+    for (size_t idx = 0; idx < sizeOfContainer; ++idx)
+        std::cout << a[idx] << " ";
+    std::cout << "\n";
+}
+
 
